f2b2d5325ed41a142af55f065d9f74c55d1195b7
Tejas Rane <tejas.m.rane@gmail.com>
2017-11-02 00:14:15 -0700

    articles started

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    Articles = [
+        {
+            'id': 1,
+            'title': '19th Century British Newspapers',
+            'body': 'Early English newspapers from London, the British Isles, and British colonies. Also included are newsbooks, acts of Parliament, addresses, broadsides, pamphlets, and proclamations. **Patrons should read the Terms & Conditions and Privacy Policy of this resource before searching.**',
+            'author': 'Gale Cengage',
+            'createDate':'11-01-2017'
+        },
+        {
+            'id': 2,
+            'title': 'African American Historical Serials Collection',
+            'body': 'Full-text digital resource exploring the history and culture of African Americans, as well as the greater Black Diaspora. Great resource for middle school, high school, and undergraduate users. Browse by type of content and by era. **Patrons should read the Privacy Policy of this resource before searching.',
+            'author': 'Rob H',
+            'createDate': '1-07-2016'
+        },
+        {
+            'id': 3,
+            'title': 'H-Net',
+            'body': 'International consortium of scholars and teachers for advancing teaching and research in the arts, humanities, and social sciences.',
+            'author': 'Carlos Sante',
+            'createDate': '1-07-2016'
+        }
+    ]
+    return Articles
