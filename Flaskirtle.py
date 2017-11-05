531b19ea0792505034ff651449b21b870816b717
Tejas Rane <tejas.m.rane@gmail.com>
2017-11-04 18:53:25 -0700

    register form

--- Flaskirtle.py
+++ Flaskirtle.py
@@ -8,6 +8,16 @@ app = Flask(__name__)
 Articles = Articles()
 
 
+#mysql
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'flaskritle'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+#init MySQL
+mysql = MySQL(app)
+
+
 @app.route('/')
 def hello_world():
     return render_template('home.html')
@@ -30,30 +40,41 @@ def article_detail(id):
 
 class RegisterForm(Form):
     name = StringField('Name', [
-        validators.input_required(),
-        validators.Length(min=1,max=50)
+        validators.Length(min=1, max=50)
     ])
-    username  = StringField('userame',[
-        validators.input_required(),
-        validators.Length(min=4,max=25)
+    username  = StringField('userame', [
+        validators.Length(min=4, max=25)
     ])
-    email = StringField('email',[
-        validators.input_required(),
+    email = StringField('email', [
         validators.Length(min=6, max=50)
     ])
     password = PasswordField('password', [
-        validators.data_required(),
+        validators.DataRequired(),
         validators.equal_to('confirm',  message='passwords do not match')
     ])
     confirm = PasswordField('confirm password')
 
 
-@app.route('/register',methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method== 'POST' and form.validate():
-        return render_template( 'register.html')
-    return render_template( 'register.html', form = form)
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        #create cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO USERS(name,username,email,password) VALUES{%s, $s, %s, %s}",(name, username, email, password))
+
+        #commit to db
+        cur.close()
+        flash('You are now registered and you can login','sucess')
+
+        redirect(url_for('index'))
+
+        #return render_template( 'register.html')
+    return render_template( 'register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
