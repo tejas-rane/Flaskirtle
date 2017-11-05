723b71c62c930d5b9be9719aea7e9a3b8896d5df
Tejas Rane <tejas.m.rane@gmail.com>
2017-11-04 19:26:35 -0700

    register form

--- Flaskirtle.py
+++ Flaskirtle.py
@@ -1,20 +1,20 @@
-from flask import Flask, render_template, flash,redirect, url_for, session, logging,request
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Articles
 from flask_mysqldb import MySQL
-from wtforms import Form,StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+
 app = Flask(__name__)
 
 Articles = Articles()
 
-
-#mysql
+# mysql
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'flaskritle'
+app.config['MYSQL_DB'] = 'flaskirtle'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-#init MySQL
+# init MySQL
 mysql = MySQL(app)
 
 
@@ -42,7 +42,7 @@ class RegisterForm(Form):
     name = StringField('Name', [
         validators.Length(min=1, max=50)
     ])
-    username  = StringField('userame', [
+    username = StringField('userame', [
         validators.Length(min=4, max=25)
     ])
     email = StringField('email', [
@@ -50,7 +50,7 @@ class RegisterForm(Form):
     ])
     password = PasswordField('password', [
         validators.DataRequired(),
-        validators.equal_to('confirm',  message='passwords do not match')
+        validators.equal_to('confirm', message='passwords do not match')
     ])
     confirm = PasswordField('confirm password')
 
@@ -58,23 +58,28 @@ class RegisterForm(Form):
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
-    if request.method== 'POST' and form.validate():
-        name = form.name.data
-        email = form.email.data
-        username = form.username.data
+    if request.method == 'POST' and form.validate():
+        name = str(form.name.data)
+        email = str(form.email.data)
+        username = str(form.username.data)
         password = sha256_crypt.encrypt(str(form.password.data))
-        #create cursor
+        print(name, username, email, password)
+        # create cursor
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO USERS(name,username,email,password) VALUES{%s, $s, %s, %s}",(name, username, email, password))
+        #query = "INSERT INTO users(name,username,email,password) VALUES{%s, %s, %s, %s}"
+        cur.execute("INSERT INTO users(name,username,email,password) VALUES(%s, %s, %s, %s)", (name, username, email, password))
 
-        #commit to db
+        # commit to db
+        mysql.connection.commit()
         cur.close()
-        flash('You are now registered and you can login','sucess')
+        flash('You are now registered and you can login', 'success')
 
-        redirect(url_for('index'))
+        #return redirect(url_for('/home'))
 
-        #return render_template( 'register.html')
-    return render_template( 'register.html', form=form)
+        #return render_template('register.html')
+    return render_template('register.html', form=form)
+
 
 if __name__ == '__main__':
+    app.secret_key = 'secret_key123'
     app.run(debug=True)
