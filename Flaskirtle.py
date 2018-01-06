2a48e94376baf37a2a6b3e4fa2f61e9a55d703c0
Tejas Rane <tejas.m.rane@gmail.com>
2018-01-05 22:03:50 -0800

    logout route

--- Flaskirtle.py
+++ Flaskirtle.py
@@ -113,6 +113,13 @@ def login():
             return render_template('login.html',error=error)
     return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
